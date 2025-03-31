ddf51f18e7341ce851ec0404d28e9ff4e5974db0
lukasg2002 <143589671+lukasg2002@users.noreply.github.com>
2025-03-31 19:57:36 +0200

    Class

---
+++ TodoList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppWPF
+{
+
+    /// <summary>
+    /// Hanterar logiken för att lägga till, ta bort och visa todo items
+    /// </summary>
+    public class TodoList
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveTask(int index) 
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+
+        public List<string> GetAllTasks() 
+        {
+            return tasks;
+        }
+    }
+}
